a9f5e6ae47d780f3f35c9c185255578f9ca7e91a
Aymane-Arfaoui <arfaouiaymane@hotmail.com>
2025-01-17 23:47:04 -0500

    components+ screeens

---
+++ mobile/app/signUp.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ScreenWrapper from '../components/ScreenWrapper'
+
+const signUp = () => {
+  return (
+    <ScreenWrapper>
+        <StatusBar />
+    <View>
+      <Text>SignUp</Text>
+    </View>
+    </ScreenWrapper>
+  )
+}
+
+export default signUp
