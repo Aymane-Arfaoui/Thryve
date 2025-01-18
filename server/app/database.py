23bfb3fb9ba88db756ba41c1130196a88555abda
“kajenthavaraj” <kajenthavaraj@MacBook-Air-641.local>
2025-01-18 12:09:03 -0500

    prototype for database

---
+++ server/app/database.py
@@ -0,0 +1,37 @@
+# {
+#   "long_term_goals": [
+#     {
+#       "description": "Achieve and maintain a healthy level of physical fitness through consistent exercise, balanced nutrition, and adequate rest."
+#     },
+#     {
+#       "description": "Develop a successful business by solving a real-world problem with a scalable product and strong customer focus."
+#     }
+#   ]
+# }
+
+
+# {
+#   "daily_actions": [
+#     {
+#       "name": "Gym",
+#       "description": "Go to the gym every day"
+#     },
+#     {
+#       "name": "Startup",
+#       "description": "Work on my startup 10 hours a day."
+#     }
+#   ]
+# }
+
+
+# {
+#   "tracked_actions": [{"2025-01-18": [{"name": "Gym", "description": "Go to the gym every day", "committed": true, "completed": true}, 
+#                                     {"name": "Startup", "description": "Work on my startup 10 hours a day", "committed": true, "completed": true}
+#                                     ], "score": -1
+#                         }, 
+#                         {"2025-01-19": [{"name": "Gym", "description": "Go to the gym every day", "committed": true, "completed": true}, 
+#                                     {"name": "Startup", "description": "Work on my startup 10 hours a day", "committed": true, "completed": true}
+#                                     ], "score": -1
+#                         }
+#   ]
+# }
