1edbb84d4136df5289287ff5406f53dda14c1eb8
Michael Liao <michaell.liao44452@gmail.com>
2025-01-19 04:55:13 +0000

    fixed bugs with call initializer

--- server/app/services/core/call_initializer.py
+++ server/app/services/core/call_initializer.py
@@ -1,6 +1,7 @@
 from app.models.context import ConversationContext
 from app.services.definitions.call_elements import CallElement
 from app.models.bot import VoiceBot
+import json
 
 class CallInitializer:
 
@@ -19,9 +20,29 @@ class CallInitializer:
         self.call_context.user_id = custom_params.pop("user_id")
         bot_id = custom_params.pop("bot_id")
 
+        print("Bot id: ", bot_id)
+
         self.call_context.bot = VoiceBot.get_bot(bot_id)
+
+        print("Bot: ", self.call_context.bot)
+
         self.call_context.const_keyword_args = custom_params
         self.call_context.const_keyword_args = start_data.get("customParameters", {})
+
+
+
+        for key, value in self.call_context.const_keyword_args.items():
+
+            print("Key: ", key, "Value: ", value)
+
+            if isinstance(value, str):
+                try:
+                    self.call_context.const_keyword_args[key] = json.loads(value.strip())
+                except Exception as e   :
+                    print("Error loading json: ", value, "Error: ", e)
+                pass
+
+        print("Const keyword args: ", self.call_context.const_keyword_args)
 
         for call_element in self.call_elements:
             await call_element.initialize_from_start_data(self.call_context)
--- server/sandbox/call_test.py
+++ server/sandbox/call_test.py
@@ -9,12 +9,14 @@ requests.post(
             "user_id" : "123",
             "bot_id" : "morning_bot",
             "first_name": "John",
-            "long_term_goals": {
-                "goal1": "Get a job",
-                "goal2": "get a girlfriend",
-                "action1": "Go to the gym",
-                "action2": "Apply to 10 jobs",
-            },
+            "goals" : """
+            Goal 1: Get a job
+            Goal 2: Get a girlfriend
+            """,
+            "actions" : """
+            Action 1: Go to the gym
+            Action 2: Apply to 10 jobs
+            """
         },
     },
 )
